203483bb2493c7c9e22b9c05f9aace458cda99e3
Denis, Oliva <tecsined@gmail.com>
2024-03-22 15:16:31 -0400

    Reduce redundant retunr parameters

--- usermanagement/user.go
+++ usermanagement/user.go
@@ -13,13 +13,10 @@ type User struct {
 	IsActive  bool
 }
 
-func (u User) ActiveAndContains(c string) (*User, bool) {
+func (u User) ActiveAndContains(c string) bool {
 	email := strings.ToLower(u.Email)
 	c = strings.ToLower(c)
-	if strings.Contains(email, c) && u.IsActive {
-		return &u, true
-	}
-	return nil, false
+	return strings.Contains(email, c) && u.IsActive
 }
 
 type Users []User
@@ -28,14 +25,14 @@ func (us Users) PrintFilteredActiveUsers(filterCriteria string) {
 	printedHeader := false
 
 	for _, user := range us {
-		filteredUser, hasMatchingEmail := user.ActiveAndContains(filterCriteria)
+		hasMatchingEmail := user.ActiveAndContains(filterCriteria)
 
 		if hasMatchingEmail {
 			if !printedHeader {
 				fmt.Printf("Active users with %s in their email:\n", filterCriteria)
 				printedHeader = true
 			}
-			fmt.Printf("ID: %d, Name: %s %s, Email: %s\n", filteredUser.Id, filteredUser.FirstName, filteredUser.LastName, filteredUser.Email)
+			fmt.Printf("ID: %d, Name: %s %s, Email: %s\n", user.Id, user.FirstName, user.LastName, user.Email)
 		}
 	}
 }
